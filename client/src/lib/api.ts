b205e832e6a7b27d4187fa5025b841f6639052c3
folckol <folckol@mail.ru>
2025-03-16 18:19:45 +0300

    Init

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import type { UUID, Character } from "@elizaos/core";
 
 const BASE_URL =
     import.meta.env.VITE_SERVER_BASE_URL ||
-    `${import.meta.env.VITE_SERVER_URL}:${import.meta.env.VITE_SERVER_PORT}`;
+    `${import.meta.env.VITE_SERVER_URL}`;
 
 console.log({ BASE_URL });
 
